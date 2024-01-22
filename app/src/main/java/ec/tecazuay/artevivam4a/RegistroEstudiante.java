f23cea5e1ef7a84c4f7974eef72784c6eaae2c1a
JhonAndresX <127310734+JhonAndresX@users.noreply.github.com>
2024-01-22 10:51:54 -0500

    Clases X

---
+++ app/src/main/java/ec/tecazuay/artevivam4a/RegistroEstudiante.java
@@ -0,0 +1,21 @@
+package ec.tecazuay.artevivam4a;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroEstudiante extends AppCompatActivity {
+Button button;
+EditText s;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_estudiant);
+    }
+    private void setup(){
+
+    }
+}
