86a9e9f6cc97d45c4f068468b54c5448292e2ad6
Juanc Idrovo <juancidrovo@gmail.com>
2024-01-27 12:41:06 -0500

    interfaz profesor, esta para buscar profe y de ahi q se liste...

---
+++ app/src/main/java/ec/tecazuay/artevivam4a/Docente.java
@@ -0,0 +1,14 @@
+package ec.tecazuay.artevivam4a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Docente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_docente);
+    }
+}
