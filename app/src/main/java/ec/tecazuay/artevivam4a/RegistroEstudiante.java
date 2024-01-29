c9ac61a1945f8d7d55086bf516ad67d8b352eff8
JhonAndresX <127310734+JhonAndresX@users.noreply.github.com>
2024-01-29 00:23:38 -0500

    cositas

--- app/src/main/java/ec/tecazuay/artevivam4a/RegistroEstudiante.java
+++ app/src/main/java/ec/tecazuay/artevivam4a/RegistroEstudiante.java
@@ -1,5 +1,7 @@
 package ec.tecazuay.artevivam4a;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -8,10 +10,12 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,13 +28,20 @@ import com.google.android.material.textfield.TextInputEditText;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class RegistroEstudiante extends AppCompatActivity {
 
     private String urlRegistro = "http://192.168.1.18:8080/api/estudiantes";
     private RequestQueue requestQueue;
     private static final int REQUEST_IMAGE_PICK = 1;
     private ImageView imageView;
-    private Uri imageUri; // Declarar como miembro de la clase
+    private Uri imageUri;
+
 
 
     @Override
@@ -38,41 +49,60 @@ public class RegistroEstudiante extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro_estudiant);
 
-        // Inicializa la RequestQueue
         requestQueue = Volley.newRequestQueue(this);
         imageView = findViewById(R.id.imageView);
         Button btnSeleccionarFoto = findViewById(R.id.btnSeleccionarFoto);
+
         btnSeleccionarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Launch the image picker
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, REQUEST_IMAGE_PICK);
             }
         });
     }
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
 
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the chosen date
+            String selectedDate = String.format(Locale.US, "%04d-%02d-%02d", year, month + 1, day);
+            ((TextInputEditText) getActivity().findViewById(R.id.txtFechaNac)).setText(selectedDate);
+        }
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_IMAGE_PICK && resultCode == RESULT_OK) {
-            // Get the selected image Uri
             imageUri = data.getData();
 
             if (imageUri != null) {
-                // Set the image to the ImageView
                 imageView.setImageURI(imageUri);
             } else {
-                // Handle the case when no image is selected
                 Toast.makeText(this, "No se ha seleccionado ninguna imagen.", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-
     public void clickbtnGuardar(View view) {
-        // Obtén los valores de los campos
         String cedula = ((TextInputEditText) findViewById(R.id.txtcedula)).getText().toString().trim();
         String nombres = ((TextInputEditText) findViewById(R.id.txtnombres)).getText().toString().trim();
         String apellidos = ((TextInputEditText) findViewById(R.id.txtapellidos)).getText().toString().trim();
@@ -80,10 +110,29 @@ public class RegistroEstudiante extends AppCompatActivity {
         String direccion = ((TextInputEditText) findViewById(R.id.txtdireccion)).getText().toString().trim();
         String telf = ((TextInputEditText) findViewById(R.id.txttelf)).getText().toString().trim();
         String contrasena = ((TextInputEditText) findViewById(R.id.txtContrasena)).getText().toString().trim();
-        // También puedes obtener la fecha de nacimiento de manera similar
+        String fechaString = ((TextInputEditText) findViewById(R.id.txtFechaNac)).getText().toString().trim();
+        Date fecha = null;
 
-        // Crea un objeto JSON con los datos
+        if (!fechaString.isEmpty()) {
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+                fecha = sdf.parse(fechaString);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                // Manejar la excepción si la cadena no se puede convertir a Date
+            }
+        }
+
         JSONObject jsonBody = new JSONObject();
+        if (fecha != null) {
+            try {
+                jsonBody.put("fecha_nac", fecha);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("RegistroEstudiante", "La fecha es null, no se añadió al JSONObject");
+        }
         try {
             jsonBody.put("cedula", cedula);
             jsonBody.put("nombres", nombres);
@@ -93,11 +142,12 @@ public class RegistroEstudiante extends AppCompatActivity {
             jsonBody.put("telf", telf);
             jsonBody.put("contrasena", contrasena);
             jsonBody.put("cedula_estudiante_fk", cedula);
+            jsonBody.put("fecha_nac", fecha);
+
             if (imageUri != null) {
                 String rutaImagen = obtenerRutaDesdeUri(imageUri);
                 jsonBody.put("foto", rutaImagen);
             } else {
-                // Handle the case when imageUri is null
                 Log.e("RegistroEstudiante", "imageUri es null en clickbtnGuardar");
             }
 
@@ -105,35 +155,37 @@ public class RegistroEstudiante extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        // Crea una solicitud POST para el registro
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, urlRegistro, jsonBody,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        // Maneja la respuesta del servidor después de un registro exitoso
-                        // Puedes implementar lógica adicional según tus necesidades
                         Log.d("RegistroEstudiante", "Respuesta del servidor: " + response.toString());
-                        // Por ejemplo, puedes navegar a la pantalla de inicio de sesión después del registro exitoso
                         startActivity(new Intent(RegistroEstudiante.this, LoginActivity.class));
-                        finish(); // Cierra la actividad actual
+                        finish();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                // Maneja el error en la solicitud
-                Log.e("RegistroEstudiante", "Error en la solicitud: " + error.getMessage());
-                Toast.makeText(RegistroEstudiante.this, "Error en la solicitud: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                String errorMessage;
+                if (error.networkResponse != null && error.networkResponse.data != null) {
+                    // Si hay datos en la respuesta, intenta convertirlos a una cadena para obtener más información.
+                    errorMessage = new String(error.networkResponse.data);
+                } else {
+                    errorMessage = "Error desconocido en la solicitud.";
+                }
+
+                Log.e("RegistroEstudiante", "Error en la solicitud: " + errorMessage, error);
+                Toast.makeText(RegistroEstudiante.this, "Error en la solicitud: " + errorMessage, Toast.LENGTH_LONG).show();
             }
         });
 
-        // Agrega la solicitud a la cola de solicitudes
         requestQueue.add(request);
     }
+
     private String obtenerRutaDesdeUri(Uri imageUri) {
         if (imageUri == null) {
-            // Manejar el caso cuando imageUri es null
             Log.e("RegistroEstudiante", "imageUri es null en obtenerRutaDesdeUri");
-            return null;  // O devuelve una cadena vacía o realiza alguna acción específica
+            return null;
         }
         String[] projection = {MediaStore.Images.Media.DATA};
         Cursor cursor = getContentResolver().query(imageUri, projection, null, null, null);
@@ -144,4 +196,3 @@ public class RegistroEstudiante extends AppCompatActivity {
         return ruta;
     }
 }
-
